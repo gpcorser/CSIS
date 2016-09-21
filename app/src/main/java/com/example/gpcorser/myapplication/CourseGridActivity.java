1c1e91c25a1754f04362e2c1fe4e1f88f9fe62ed
gpcorser <gpcorser@svsu.edu>
2016-09-21 16:22:04 -0400

    added 3 activities

---
+++ app/src/main/java/com/example/gpcorser/myapplication/CourseGridActivity.java
@@ -0,0 +1,13 @@
+package com.example.gpcorser.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_grid);
+    }
+}
